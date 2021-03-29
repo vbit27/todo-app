559ff0e9535e4d7cd533c92b46459c85364024e9
Vasil Bit <vasil.bituni@gmail.com>
2021-03-29 13:11:03 +0200

    Add change active project

--- src/DynamicListener.js
+++ src/DynamicListener.js
@@ -1,12 +1,17 @@
-import { deleteProject } from './Logic'
+import { deleteProject, setActiveProject } from './Logic'
 
 function addProjectListeners() {
     const projectElements = document.querySelectorAll('.single-project-container h3')
     const deleteProjectBtn = document.querySelectorAll('.delete-project-btn');
 
+
+    projectElements.forEach((project) => { project.addEventListener('click', function() {
+        let index = this.parentNode.getAttribute('data-index');
+        setActiveProject(index);
+    })})
+    
     deleteProjectBtn.forEach((project) => {project.addEventListener('click', function(){
-        let index =  this.parentNode.getAttribute('data-index')
-        
+        let index = this.parentNode.getAttribute('data-index');
         deleteProject(index)
     })})
 }
--- src/Logic.js
+++ src/Logic.js
@@ -6,12 +6,9 @@ import { renderProjcts } from './render'
 const createNewProject = (name, description) => {
     resetActiveProject()
     allProjects.push(newProject(name, description))
-
 }
 
-
 const deleteProject = (index) => {
-
     if (allProjects[index].active) {
         allProjects[0].active = true;
     }
@@ -24,7 +21,9 @@ const resetActiveProject = () => {
     allProjects.forEach(project => project.active = false)
 }
 
+const setActiveProject = (index) => {
+    resetActiveProject()
+    allProjects[index].active = true;
+}
 
-
-
-export { createNewProject, deleteProject }
+export { createNewProject, deleteProject, setActiveProject }
